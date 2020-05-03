ecc196054910d257bf622ac04839220a4b3ef957
Kurt Danilson <Kurt Danilson@LAPTOP-0RSIG2RT>
2020-05-03 16:08:55 -0700

    Updated info in println

--- Lab04/src/Hello.java
+++ Lab04/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information.
 		// Add your SacState ID and your First name Last name as indicated
 		System.out.println("218846655 - This line was added by Ivince Pajarin.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301701374 - This line was added by Kurt Danilson.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
