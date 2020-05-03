884ba8247314b5c67d82db41d139476deb59c237
ivinc <ivinc@DESKTOP-OHLCOK2.attlocal.net>
2020-05-03 15:55:38 -0700

    Added new comment line

--- Lab04/src/Hello.java
+++ Lab04/src/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
-		// Add a new line with your information
+		// Add a new line with your information.
+		// Add your SacState ID and your First name Last name as indicated
 		System.out.println("218846655 - This line was added by Ivince Pajarin.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
