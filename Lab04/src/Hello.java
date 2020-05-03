5004ea504aae39d167ac9165ad039b246406991b
kimhiengtiv <kimhiengtiv@localhost>
2020-05-03 16:33:43 -0700

    Changes made by Kimhieng Tiv

--- Lab04/src/Hello.java
+++ Lab04/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		// Add your SacState ID and your First name Last name as indicated
 		System.out.println("218846655 - This line was added by Ivince Pajarin.");
 		System.out.println("301701374 - This line was added by Kurt Danilson.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("216370779 - This line was added by Kimhieng Tiv.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
